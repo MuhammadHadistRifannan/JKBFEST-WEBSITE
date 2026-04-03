7aa7cd277d5ada78fcbfa07131300145a6eaa8fd
Ilham Budi Trisetyo <ilhamtrisetyo@gmail.com>
2026-04-03 18:09:25 +0700

    remove requirement format reset password

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -217,7 +217,7 @@ class UserService
     {
         $validated = $request->validate([
             'email' => 'required|email',
-            'password' => 'required|min:8|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).+$/',
+            'password' => 'required|min:8',
             'password_confirmation' => 'required|same:password',
             'token' => 'required'
         ]);
